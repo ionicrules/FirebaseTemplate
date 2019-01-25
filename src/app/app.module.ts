95af20a1f5124ca521366e4626382611bf6879e4
Hernán Castañón <hernanderes34@gmail.com>
2019-01-25 13:12:29 +0100

    Track firebase keys from environment
    We use app.module.ts and AngularFirebase to use firebase keys from
    environment.
    
    Signed-off-by: Hernán Castañón <hernanderes34@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { Firebase } from '@ionic-native/firebase/ngx';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 
+import {Firebase} from '@ionic-native/firebase/ngx';
 import {FcmService} from './fcm.service';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -25,4 +34,5 @@ import {FcmService} from './fcm.service';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
