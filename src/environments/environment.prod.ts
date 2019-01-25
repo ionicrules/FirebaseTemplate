258e9af82597ed1725f7bccdb9498d1763e096a5
Hernán Castañón <hernanderes34@gmail.com>
2019-01-25 13:08:03 +0100

    Add Firebase API keys
    Use environment.prod for release version, environment for debug.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // Keys for release version a.k.a final API keys.
+  firebase : {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  }
 };
