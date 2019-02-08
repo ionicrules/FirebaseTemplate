8bf65b9dbd86ae6b9187025cf2aacf4266117342
Hernán Castañón <hernanderes34@gmail.com>
2019-02-08 09:04:31 +0100

    Initializate Firebase Notification Service on app boot
    Once the app has been launched, we will load firebase and the
    notification service.
    
    With this, notifications will have full access to the app, so it is not
    page dependant.
    
    If we are inside the application it will bring us a toastcontroller,
    will send a notification if the app is on background or not opened.
    
    Signed-off-by: Hernán Castañón <hernanderes34@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FcmService } from './fcm.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FcmService,
+    private toastCtrl: ToastController
   ) {
     this.initializeApp();
   }
@@ -21,6 +25,28 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.loadFirebase();
     });
+  }
+
+  loadFirebase() {
+    console.log("hola");
+    // Get a FCM token
+    this.fcm.getToken()
+
+    // Listen to incoming messages
+    this.fcm.listenToNotifications().pipe(
+        tap(async msg => {
+          // show a toast
+          console.log("Me ha llegado notificacion")
+          const toast = await this.toastCtrl.create({
+            message: msg.body,
+            duration: 3000
+          });
+          return await toast.present();
+        })
+    )
+        .subscribe()
   }
 }
